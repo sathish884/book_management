b4e358497d0f10688e0fb07784c305d85f789149
suthan-raja <suthanraja190@gmail.com>
2025-09-15 11:39:48 +0530

    Latest

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -3,8 +3,13 @@ const dotenv = require("dotenv");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Database Connected");
+    console.log("RAW MONGO_URI:", JSON.stringify(process.env.MONGO_URI));
+
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "book_management",
+    });
+
+    console.log("Database Connected to:", mongoose.connection.name);
   } catch (error) {
     console.error("Database Connection Failed:", error.message);
     process.exit(1);
