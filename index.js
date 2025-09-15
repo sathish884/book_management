1b17e6d94d14b0a962f4b0e60e823a82f2e899c2
suthan-raja <suthanraja190@gmail.com>
2025-09-15 11:24:04 +0530

    updates

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const userRouter = require('./routes/UserRoutes');
 const bookRoutes = require('./routes/BooksRoutes');
-
-//const envFile = path.resolve(process.cwd(), "env", `.env.${process.env.NODE_ENV || "development"}`);
-
-//console.log("Loaded ENV from:", envFile); // DEBUG
-//console.log("MONGO_URI =", process.env.MONGO_URI); // DEBUG
-
-//"prod": "cross-env NODE_ENV=production nodemon index.js",
-//    "test": "cross-env NODE_ENV=test nodemon index.js"
-//if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-//}
-
+require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
@@ -32,9 +21,9 @@ const PORT = process.env.PORT || 8080;
 connectDB();
 
 app.get("/", (req, res) => {
-    res.send(`Hello from ${process.env.NODE_ENV} environment`);
+  res.send(`Hello from ${process.env.NODE_ENV} environment`);
 });
 
 app.listen(PORT, () => {
-    console.log(`[${process.env.NODE_ENV}] Server is running on port ${PORT}`);
+  console.log(`[${process.env.NODE_ENV}] Server is running on port ${PORT}`);
 });
